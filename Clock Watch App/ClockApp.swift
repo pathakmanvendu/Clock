cb7d3777dd88c94d5badf20f308e4a949857732d
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-30 11:43:28 +0530

    Initial Commit

---
+++ Clock Watch App/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock Watch App
+//
+//  Created by manvendu pathak  on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Clock_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
